e96c418e2f83303950c7a427aaef04389ffac62f
Aldrin K. Mendoza Fernandez <amendoza@tranred.com.ve>
2022-02-08 11:15:33 -0400

    Modificacion de Tasa a 4 decimales

--- src/functions/history.ts
+++ src/functions/history.ts
@@ -162,7 +162,7 @@ export const selects: select[] = [
 		key: 'MONTO_ABONAR',
 		query: `Format([MONTO_ABONAR], 'N2', 'es-es') as [MONTO_ABONAR]`,
 	},
-	{ key: 'TASA', query: `Format(CONVERT(float, [TASA]), 'N2', 'es-es') as [TASA]` },
+	{ key: 'TASA', query: `[TASA]` },
 	{ key: 'CANT_TRANSACCION', query: '[CANT_TRANSACCION]' },
 	{ key: 'TIPO_DE_CARTERA', query: '[TIPO_DE_CARTERA]' },
 	{
