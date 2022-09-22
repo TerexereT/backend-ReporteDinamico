9b2fb30968a3a0b6529e726802e1d7ff3cab98d6
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-09-22 12:04:44 -0400

    local

--- src/index.ts
+++ src/index.ts
@@ -25,18 +25,6 @@ createConnection()
 
 		Routes(app);
 
-		// register express routes from defined application routes
-		// Routes.forEach((route) => {
-		// 	(app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-		// 		const result = new (route.controller as any)()[route.action](req, res, next);
-		// 		if (result instanceof Promise) {
-		// 			result.then((result) => (result !== null && result !== undefined ? res.send(result) : undefined));
-		// 		} else if (result !== null && result !== undefined) {
-		// 			res.json(result);
-		// 		}
-		// 	});
-		// });
-
 		posRoutes(app);
 
 		// setup express app here
